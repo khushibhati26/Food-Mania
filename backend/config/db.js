25216e9adc9a31edeff1a43aa753a68e0c41676a
Khushi Bhati <kb242611@gmail.com>
2025-05-23 22:29:30 +0530

    env updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load env variables
 
 export const connectDB = async () => {
+  const mongoURI = process.env.MONGO_URI;
+  if (!mongoURI) {
+    throw new Error("MONGO_URI not found in .env file");
+  }
+
   await mongoose
-    .connect(
-      "mongodb+srv://manaswirajsharma1001:aRvwsrT4saQsSl2j@cluster0.tyyv761.mongodb.net/tomato"
-    )
-    .then(() => console.log("DB connected"));
+    .connect(mongoURI)
+    .then(() => console.log("DB connected"))
+    .catch((err) => {
+      console.error("DB connection error:", err);
+    });
 };
